073e0365b4ae0879aab8e7ba27f9fa7dadb21a8b
rajwinder <sahotafarm778@gmail.com>
2025-06-25 17:15:05 +0300

    maths function added

---
+++ maths-function.php
@@ -0,0 +1,22 @@
+<?php
+function add(){
+    $a = 5;
+    $b = 10;
+    return $a + $b;
+}
+function sub(){
+    $a = 5;
+    $b = 10;
+    return $a - $b;
+}
+function mul(){
+    $a = 5;
+    $b = 10;
+    return $a * $b;
+}
+function div(){
+    $a = 5;
+    $b = 10;
+    return $a / $b;
+}
+?>
